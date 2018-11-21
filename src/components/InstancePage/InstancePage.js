5abc75f3a58c91129b2f587ac2f2d2393a780a5a
Jonathan Reckinger <jpreckinger@gmail.com>
2018-11-21 09:32:48 -0600

    completed adding a game to the database from API

--- src/components/InstancePage/InstancePage.js
+++ src/components/InstancePage/InstancePage.js
@@ -6,15 +6,14 @@ import InstanceCard from '../InstanceCard/InstanceCard';
 class InstancePage extends Component {
 
     startGameClick = () => {
-        // this.props.history.push('/results');
-        console.log('reduxstate', this.props.reduxState.prepareInstance);
+        this.props.history.push('/results');
     }
 
     render() {
         return (
             <div>   
                 <button onClick={this.startGameClick}>Start Game</button>
-                {this.props.reduxState.prepareInstance.map( (game, index) => (
+                {this.props.prepareInstance.map( (game, index) => (
                     <div key={index}>
                         <InstanceCard game={game} />
                     </div>
@@ -27,7 +26,7 @@ class InstancePage extends Component {
 // Instead of taking everything from state, we just want the error messages.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({errors}) => ({ errors });
-const mapStateToProps = reduxState => ({reduxState});
+const mapStateToProps = ({prepareInstance}) => ({prepareInstance});
 
 export default connect(mapStateToProps)(InstancePage);
 
--- src/redux/reducers/prepareInstanceReducer.js
+++ src/redux/reducers/prepareInstanceReducer.js
@@ -1,7 +1,6 @@
 const prepareInstance = (state = [], action) => {
     switch (action.type) {
       case 'DISPLAY_CURRENT_GAME':
-        console.log('in displaycurrentgame');
         return  action.payload;
       default:
         return state;
