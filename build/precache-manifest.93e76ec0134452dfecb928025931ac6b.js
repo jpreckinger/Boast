aeea45e7d7548161fefd1e13eaa9575e09f7a3e8
Jonathan Reckinger <jpreckinger@gmail.com>
2018-12-03 21:26:24 -0600

    build for heroku

---
+++ build/precache-manifest.93e76ec0134452dfecb928025931ac6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56bc312a159a84762454",
+    "url": "/static/js/main.56bc312a.chunk.js"
+  },
+  {
+    "revision": "a88d8b159a33acbf1197",
+    "url": "/static/js/1.a88d8b15.chunk.js"
+  },
+  {
+    "revision": "56bc312a159a84762454",
+    "url": "/static/css/main.68dfa97c.chunk.css"
+  },
+  {
+    "revision": "a88d8b159a33acbf1197",
+    "url": "/static/css/1.026d2472.chunk.css"
+  },
+  {
+    "revision": "bc73c5f81a1b8d72b0f313758434155b",
+    "url": "/index.html"
+  }
+];
